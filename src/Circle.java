75efffd0216f8320fccc6747c9d539409a32dd9f
TheSpiceyCatto <77725193+TheSpiceyCatto@users.noreply.github.com>
2023-05-29 23:46:11 +0200

    Created classes

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,17 @@
-public class Circle {
+import javax.swing.*;
+import java.awt.*;
+
+public class Circle extends JComponent {
+    int x, y, r;
+    public Circle(int x, int y){
+        this.x = x;
+        this.y = y;
+        r = 100;
+        setPreferredSize(new Dimension(r, r));
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        g.fillOval(x, y, r, r);
+    }
 }
--- src/Project.java
+++ src/Project.java
@@ -1,9 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
-import java.security.Key;
 
 public class Project {
     public static void main(String[] args) {
@@ -43,13 +41,13 @@ public class Project {
         JMenu jMenuFile = new JMenu("File");
 
         JMenuItem openItem = new JMenuItem("Open", KeyEvent.VK_O);
-        openItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
+        openItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_DOWN_MASK));
         JMenuItem saveItem = new JMenuItem("Save", KeyEvent.VK_S);
-        saveItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+        saveItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
         JMenuItem saveAsItem = new JMenuItem("Save As...", KeyEvent.VK_A);
-        saveAsItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK + InputEvent.ALT_DOWN_MASK));
+        saveAsItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK + InputEvent.ALT_DOWN_MASK));
         JMenuItem quitItem = new JMenuItem("Quit", KeyEvent.VK_Q);
-        quitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.CTRL_MASK));
+        quitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, InputEvent.CTRL_DOWN_MASK));
 
         jMenuFile.add(openItem);
         jMenuFile.add(saveItem);
@@ -64,13 +62,13 @@ public class Project {
         figureMenu.setMnemonic(KeyEvent.VK_F);
         JRadioButtonMenuItem circleButton = new JRadioButtonMenuItem("Circle");
         circleButton.setMnemonic(KeyEvent.VK_C);
-        circleButton.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
+        circleButton.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_DOWN_MASK));
         JRadioButtonMenuItem squareButton = new JRadioButtonMenuItem("Square");
         squareButton.setMnemonic(KeyEvent.VK_R);
-        squareButton.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R, ActionEvent.CTRL_MASK));
+        squareButton.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R, InputEvent.CTRL_DOWN_MASK));
         JRadioButtonMenuItem penButton = new JRadioButtonMenuItem("Pen");
         penButton.setMnemonic(KeyEvent.VK_P);
-        penButton.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.CTRL_MASK));
+        penButton.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, InputEvent.CTRL_DOWN_MASK));
         buttonGroup.add(circleButton);
         buttonGroup.add(squareButton);
         buttonGroup.add(penButton);
@@ -78,9 +76,9 @@ public class Project {
         figureMenu.add(squareButton);
         figureMenu.add(penButton);
         JMenuItem colorItem = new JMenuItem("Color", KeyEvent.VK_C);
-        colorItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.ALT_MASK + InputEvent.SHIFT_DOWN_MASK));
+        colorItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.ALT_DOWN_MASK + InputEvent.SHIFT_DOWN_MASK));
         JMenuItem clearItem = new JMenuItem("Clear", KeyEvent.VK_C);
-        clearItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK + InputEvent.SHIFT_DOWN_MASK));
+        clearItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK + InputEvent.SHIFT_DOWN_MASK));
 
         jMenuDraw.add(figureMenu);
         jMenuDraw.add(colorItem);
