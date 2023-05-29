1a83abedeac3ac6a1d219499aafc0358c2a295d2
TheSpiceyCatto <77725193+TheSpiceyCatto@users.noreply.github.com>
2023-05-29 23:21:37 +0200

    Added UI

---
+++ src/Project.java
@@ -0,0 +1,94 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.security.Key;
+
+public class Project {
+    public static void main(String[] args) {
+        new Project();
+    }
+
+    private Project(){
+        SwingUtilities.invokeLater(this::createGUI);
+    }
+
+    private void createGUI(){
+        JFrame jFrame = new JFrame("Simple Draw");
+        jFrame.setLayout(new BorderLayout());
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JMenuBar jMenuBar = createMenu();
+
+        JToolBar statusBar = new JToolBar();
+        statusBar.setLayout(new BorderLayout());
+        JLabel shapeStatus = new JLabel("Status");
+        JLabel fileStatus = new JLabel("New");
+        statusBar.add(shapeStatus, BorderLayout.WEST);
+        statusBar.add(fileStatus, BorderLayout.EAST);
+
+        JPanel drawArea = new JPanel();
+
+        jFrame.add(drawArea, BorderLayout.CENTER);
+        jFrame.add(statusBar, BorderLayout.SOUTH);
+        jFrame.setJMenuBar(jMenuBar);
+        jFrame.setSize(500, 500);
+        jFrame.setResizable(true);
+        jFrame.setVisible(true);
+    }
+
+    private JMenuBar createMenu(){
+        JMenuBar jMenuBar = new JMenuBar();
+        JMenu jMenuFile = new JMenu("File");
+
+        JMenuItem openItem = new JMenuItem("Open", KeyEvent.VK_O);
+        openItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
+        JMenuItem saveItem = new JMenuItem("Save", KeyEvent.VK_S);
+        saveItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+        JMenuItem saveAsItem = new JMenuItem("Save As...", KeyEvent.VK_A);
+        saveAsItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK + InputEvent.ALT_DOWN_MASK));
+        JMenuItem quitItem = new JMenuItem("Quit", KeyEvent.VK_Q);
+        quitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.CTRL_MASK));
+
+        jMenuFile.add(openItem);
+        jMenuFile.add(saveItem);
+        jMenuFile.add(saveAsItem);
+        jMenuFile.addSeparator();
+        jMenuFile.add(quitItem);
+
+        JMenu jMenuDraw = new JMenu("Draw");
+
+        JMenu figureMenu = new JMenu("Figure");
+        ButtonGroup buttonGroup = new ButtonGroup();
+        figureMenu.setMnemonic(KeyEvent.VK_F);
+        JRadioButtonMenuItem circleButton = new JRadioButtonMenuItem("Circle");
+        circleButton.setMnemonic(KeyEvent.VK_C);
+        circleButton.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
+        JRadioButtonMenuItem squareButton = new JRadioButtonMenuItem("Square");
+        squareButton.setMnemonic(KeyEvent.VK_R);
+        squareButton.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R, ActionEvent.CTRL_MASK));
+        JRadioButtonMenuItem penButton = new JRadioButtonMenuItem("Pen");
+        penButton.setMnemonic(KeyEvent.VK_P);
+        penButton.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.CTRL_MASK));
+        buttonGroup.add(circleButton);
+        buttonGroup.add(squareButton);
+        buttonGroup.add(penButton);
+        figureMenu.add(circleButton);
+        figureMenu.add(squareButton);
+        figureMenu.add(penButton);
+        JMenuItem colorItem = new JMenuItem("Color", KeyEvent.VK_C);
+        colorItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.ALT_MASK + InputEvent.SHIFT_DOWN_MASK));
+        JMenuItem clearItem = new JMenuItem("Clear", KeyEvent.VK_C);
+        clearItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK + InputEvent.SHIFT_DOWN_MASK));
+
+        jMenuDraw.add(figureMenu);
+        jMenuDraw.add(colorItem);
+        jMenuDraw.addSeparator();
+        jMenuDraw.add(clearItem);
+
+        jMenuBar.add(jMenuFile);
+        jMenuBar.add(jMenuDraw);
+        return jMenuBar;
+    }
+}
